7d4b0ff0c5b24e1027ff4e01897708729e0ce250
Liss Heidrich <31625940+liss-h@users.noreply.github.com>
2025-06-12 13:06:03 +0200

    make generate script more robust and more maintainable

--- generate_operations.py
+++ generate_operations.py
@@ -1,174 +1,229 @@
-import sys
-import requests
-from pathlib import Path
-import random
-import os
+import argparse
+import dataclasses
+import enum
 import json
+import os
+import random
+import rdflib
+import requests
+import typing
 
+# additional options (hard coded for now)
+N_WORKERS: int = 4
+N_OPERATIONS: int = 20
+DELETE_DATA_TEMPLATE_SIZE_MIN: int = 1  # min number of triples to be deleted by a DELETE DATA operation
+DELETE_DATA_TEMPLATE_SIZE_MAX: int = 10  # max number of triples to be deleted by a DELETE DATA operation
+INSERT_DATA_TEMPLATE_SIZE_MIN: int = 1  # min number of triples to be inserted by an INSERT DATA operation
+INSERT_DATA_TEMPLATE_SIZE_MAX: int = 10  # max number of triples to be inserted by an INSERT DATA operation
+TYPE_OF_SUBJECTS: rdflib.URIRef = rdflib.URIRef("http://xmlns.com/foaf/0.1/Person")
+INSERT_PREDICATE: rdflib.URIRef = rdflib.URIRef("http://www.example.org/test")
+
+Graph = str
+
+class OperationKind(enum.Enum):
+    INSERT_DATA = 0
+    DELETE_DATA = 1
+    GSP_POST = 2
+    GSP_PUT = 3
+    GSP_DELETE = 4
+
+@dataclasses.dataclass
+class UpdateOperation:
+    subject: str
+    operation: OperationKind
+    triples: typing.Optional[Graph]
+    validation_default: str
+    validation_named: str
+
+    def to_json(self):
+        return json.dumps({
+            "subject": self.subject,
+            "operation": self.operation.name,
+            "triples": self.triples,
+            "validation_default": self.validation_default,
+            "validation_named": self.validation_named,
+        }, indent=4)
+
+class RDFStore:
+    def __init__(self, query_endpoint_url: str, update_endpoint_url: str, graph_store_endpoint_url: str):
+        self.query_endpoint_url = query_endpoint_url
+        self.update_endpoint_url = update_endpoint_url
+        self.graph_store_endpoint_url = graph_store_endpoint_url
+
+    @staticmethod
+    def get(**kwargs) -> str:
+        resp = requests.get(**kwargs)
+        resp.raise_for_status()
+        return resp.text
+
+    @staticmethod
+    def post(**kwargs):
+        requests.post(**kwargs).raise_for_status()
+
+    @staticmethod
+    def put(**kwargs):
+        requests.put(**kwargs).raise_for_status()
+
+    @staticmethod
+    def delete(**kwargs):
+        requests.delete(**kwargs).raise_for_status()
+
+    def validation_default_graph(self, ident: rdflib.URIRef) -> str:
+        query = f"CONSTRUCT {{ {ident.n3()} ?p ?o }} WHERE {{ {ident.n3()} ?p ?o . FILTER(!isBlank(?o)) }}"
+        return self.get(url=self.query_endpoint_url, params={"query": query}, headers={"Accept": "application/n-triples"})
+
+    def validation_named_graph(self, ident: rdflib.URIRef) -> str:
+        query = f"CONSTRUCT {{ {ident.n3()} ?p ?o }} WHERE {{ GRAPH {ident.n3()} {{ {ident.n3()} ?p ?o . FILTER(!isBlank(?o)) }} }}"
+        return self.get(url=self.query_endpoint_url, params={"query": query}, headers={"Accept": "application/n-triples"})
+
+    def reset(self, triples_file: str):
+        self.post(url=self.update_endpoint_url, data="DROP ALL", headers={"Content-type": "application/sparql-update"})
+
+        with open(triples_file, "rb") as f:
+            self.put(url=self.graph_store_endpoint_url, params="default", data=f,
+                     headers={"Content-Type": "application/n-triples"})
+
+    def insert_data(self, ident: rdflib.URIRef, triples: Graph):
+        update = f"""
+            INSERT DATA {{ {triples} }};
+            INSERT DATA {{ GRAPH {ident.n3()} {{ {triples} }} }}
+        """
+
+        self.post(url=self.update_endpoint_url, data=update,
+                  headers={"Content-type": "application/sparql-update"})
+
+    def delete_data(self, ident: rdflib.URIRef, number_of_triples: int) -> str:
+        construct_query = f"CONSTRUCT {{ {ident.n3()} ?p ?o }} WHERE {{ {ident.n3()} ?p ?o . FILTER(!isBlank(?o)) }} LIMIT {number_of_triples}"
+        graph = self.get(url=self.query_endpoint_url, params={"query": construct_query},
+                         headers={"Accept": "application/n-triples"})
+
+        query_default = f"DELETE DATA {{ {graph} }}"
+        self.post(url=self.update_endpoint_url, data=query_default,
+                      headers={"Content-type": "application/sparql-update"})
+
+        return graph
+
+    def gsp_post(self, ident: rdflib.URIRef, triples: Graph):
+        self.post(url=self.graph_store_endpoint_url, params="default", data=triples,
+                  headers={"Content-type": "application/n-triples"})
+        self.post(url=self.graph_store_endpoint_url, params={"graph": ident}, data=triples,
+                  headers={"Content-type": "application/n-triples"})
+
+    def gsp_put(self, ident: rdflib.URIRef, triples: Graph):
+        self.put(url=self.graph_store_endpoint_url, params={"graph": ident}, data=triples,
+                 headers={"Content-type": "application/n-triples"})
+
+    def gsp_delete(self, ident: rdflib.URIRef):
+        self.delete(url=self.graph_store_endpoint_url, params={"graph": ident})
+
+
+def extract_subjects_from_graph(triples_file: str) -> typing.List[rdflib.URIRef]:
+    g = rdflib.Graph()
+    g.parse(triples_file)
+
+    return [s for s, p, o in g if isinstance(s, rdflib.URIRef) and o == TYPE_OF_SUBJECTS]
+
+def generate_triples(number_of_triples: int, ident: rdflib.URIRef, counter: int) -> Graph:
+    g = rdflib.Graph()
+    for i in range(0, number_of_triples):
+        g.add((ident, INSERT_PREDICATE, rdflib.URIRef(f"http://www.example.org/test/{counter}")))
+
+    return g.serialize(format="nt")
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("n_triples_file", type=str)
+    parser.add_argument("output_directory", type=str)
+    parser.add_argument("query_endpoint_url", type=str)
+    parser.add_argument("update_endpoint_url", type=str)
+    parser.add_argument("graph_store_endpoint_url", type=str)
+    args = parser.parse_args()
 
-    if len(sys.argv) != 6:
-        print("Exprected arguments: n-triples file, output directory, endpoint url (read), update endpoint url, and graph store endpoint url")
-        exit(1)
-    
-    # collect cmd arguments
-    N_TRIPLES_FILE = sys.argv[1]
-    OUTPUT_DIRECTORY = sys.argv[2]
-    ENDPOINT_URL = sys.argv[3]
-    UPDATE_ENDPOINT_URL = sys.argv[4]
-    GRAPH_STORE_ENDPOINT_URL = sys.argv[5]
-
-    # additional options (hard coded for now)
-    N_WORKERS = 4
-    N_OPERATIONS = 20
-    POSSIBLE_OPERATIONS = 5
-    DELETE_DATA_TEMPLATE_SIZE_MIN = 1  # min number of triples to be deleted by a DELETE DATA operation
-    DELETE_DATA_TEMPLATE_SIZE_MAX = 10 # max number of triples to be deleted by a DELETE DATA operation
-    INSERT_DATA_TEMPLATE_SIZE_MIN = 1  # min number of triples to be inserted by an INSERT DATA operation
-    INSERT_DATA_TEMPLATE_SIZE_MAX = 10 # max number of triples to be inserted by an INSERT DATA operation
-    TYPE_OF_SUBJECTS="<http://xmlns.com/foaf/0.1/Person>"
-    INSERT_PREDICATE="<http://www.example.org/test>"
-    INSERT_INTEGER=0
-
-    def reset_endpoint(input_file):
-        requests.post(url=UPDATE_ENDPOINT_URL, data="DROP ALL", headers={"Content-type": "application/sparql-update"})
-        requests.post(url=UPDATE_ENDPOINT_URL, data=f"LOAD <file://{input_file}>", headers={"Content-type": "application/sparql-update"})
-
-    def generate_triples(number_of_triple, subject, counter):
-        ntriples = ""
-        for i in range(0, number_of_triples):
-            ntriples += f"{subject} {INSERT_PREDICATE} <http://www.example.org/test/{counter}> ."
-            counter += 1
-        return ntriples
-
-    def validation_default_graph(subject):
-        query = f"CONSTRUCT {{ {subject} ?p ?o }} WHERE {{ {subject} ?p ?o . FILTER(!isBlank(?o)) }}"
-        return requests.get(url=ENDPOINT_URL, params={"query": query}, headers={"Accept": "text/plain"}).text.replace('\n', ' ')
-
-    def validation_named_graph(subject):
-        query = f"CONSTRUCT {{ {subject} ?p ?o }} WHERE {{ GRAPH {subject} {{ {subject} ?p ?o . FILTER(!isBlank(?o)) }} }}"
-        return requests.get(url=ENDPOINT_URL, params={"query": query}, headers={"Accept": "text/plain"}).text.replace('\n', ' ')
-
-    def insert_data(subject, graph):
-        query_default = f"INSERT DATA {{ {graph} }}"
-        query_named = f"INSERT DATA {{ GRAPH {subject} {{ {graph} }} }}"
-        requests.post(url=UPDATE_ENDPOINT_URL, data=query_default, headers={"Content-type": "application/sparql-update"})
-        requests.post(url=UPDATE_ENDPOINT_URL, data=query_named, headers={"Content-type": "application/sparql-update"})
-
-    def gsp_post(subject, graph):
-        requests.post(url=f"{GRAPH_STORE_ENDPOINT_URL}?default", data=graph, headers={"Content-type": "application/n-triples"})
-        requests.post(url=f"{GRAPH_STORE_ENDPOINT_URL}?graph={subject[1:-1]}", data=graph, headers={"Content-type": "application/n-triples"})
-
-    def gsp_put(subject, graph):
-        requests.put(url=f"{GRAPH_STORE_ENDPOINT_URL}?graph={subject[1:-1]}", data=graph, headers={"Content-type": "application/n-triples"})
-    
-    def gsp_delete(subject):
-        requests.delete(url=f"{GRAPH_STORE_ENDPOINT_URL}?graph={subject[1:-1]}")
-
-    def get_triples_for_delete_data(subject, number_of_triples):
-        query = f"CONSTRUCT {{ {subject} ?p ?o }} WHERE {{ {subject} ?p ?o . FILTER(!isBlank(?o)) }} LIMIT {number_of_triples}"
-
-    def delete_data(number_of_triples):
-        construct_query = f"CONSTRUCT {{ {subject} ?p ?o }} WHERE {{ {subject} ?p ?o . FILTER(!isBlank(?o)) }} LIMIT {number_of_triples}"
-        graph = requests.get(url=ENDPOINT_URL, params={"query": construct_query}, headers={"Accept": "text/plain"}).text.replace('\n', ' ')
-        query_default = f"DELETE DATA {{ {graph} }}"
-        requests.post(url=UPDATE_ENDPOINT_URL, data=query_default, headers={"Content-type": "application/sparql-update"})
-
+    rdfstore = RDFStore(
+        query_endpoint_url=args.query_endpoint_url,
+        update_endpoint_url=args.update_endpoint_url,
+        graph_store_endpoint_url=args.graph_store_endpoint_url,
+    )
 
     # delete existing data and load the ntriples file in the remote endpoint
-    reset_endpoint(os.path.abspath(N_TRIPLES_FILE))
+    rdfstore.reset(args.n_triples_file)
 
     # get all unique subjects of the provided graph
-    subjects = list()
-    with open(N_TRIPLES_FILE, "r") as input_graph:
-        for line in input_graph:
-            triple = line.split(' ')
-            # skip triples that are not ?s ?p <http://xmlns.com/foaf/0.1/Person>
-            if len(triple) < 3 or triple[2] != TYPE_OF_SUBJECTS:
-                continue
-            subjects.append(triple[0])
+    subjects = extract_subjects_from_graph(args.n_triples_file)
+    subject_index = 0
 
-    sujbect_index = 0
+    insert_integer = 0
+
+    try:
+        os.mkdir(args.output_directory)
+    except:
+        # allow directory exists
+        pass
 
     for w_idx in range(0, N_WORKERS):
         # create worker directory
-        worker_dir = f"{OUTPUT_DIRECTORY}/worker_{w_idx}"
-        Path(worker_dir).mkdir(parents=True, exist_ok=True)
+        worker_dir = f"{args.output_directory}/worker_{w_idx}"
+        os.mkdir(worker_dir)
+
         for o_idx in range(0, N_OPERATIONS):
             # randomly select an operation
-            op = None
-            if sujbect_index + o_idx == 0:
-                # first subject, always start with insert data
-                op = 0
-            else:
-                op = random.randrange(0, POSSIBLE_OPERATIONS)
 
-            ntriples_for_operation = ""
-            operation_name = ""
-            subject = ""
-            
+            # first subject, always start with insert data
+            op_kind = OperationKind.INSERT_DATA if subject_index + o_idx == 0 else random.choice(list(OperationKind))
+
             # INSERT DATA
-            if op == 0:
-                subject = subjects[sujbect_index + o_idx]
+            if op_kind == OperationKind.INSERT_DATA:
+                subject = subjects[subject_index + o_idx]
                 number_of_triples = random.randrange(INSERT_DATA_TEMPLATE_SIZE_MIN, INSERT_DATA_TEMPLATE_SIZE_MAX)
-                ntriples_for_operation = generate_triples(number_of_triples, subject, INSERT_INTEGER)
-                INSERT_INTEGER += number_of_triples
-                operation_name = "INSERT_DATA"
-                insert_data(subject, ntriples_for_operation)
+                ntriples_for_operation = generate_triples(number_of_triples, subject, insert_integer)
+                insert_integer += number_of_triples
+                rdfstore.insert_data(subject, ntriples_for_operation)
             
             # DELETE DATA
-            elif op == 1:
-                # the subject that will be selected has not been used before
-                # we can get the triples to be removed using a construct query
-                # this is done in the function `delete_data`
-                subject = subjects[sujbect_index + o_idx]
+            elif op_kind == OperationKind.DELETE_DATA:
+                subject = subjects[subject_index + o_idx]
                 number_of_triples = random.randrange(DELETE_DATA_TEMPLATE_SIZE_MIN, DELETE_DATA_TEMPLATE_SIZE_MAX)
-                operation_name = "DELETE_DATA"
-                delete_data(number_of_triples)
+                ntriples_for_operation = rdfstore.delete_data(subject, number_of_triples)
             
             # Graph Store Protocol: POST
-            elif op == 2:
-                subject = subjects[sujbect_index + o_idx]
+            elif op_kind == OperationKind.GSP_POST:
+                subject = subjects[subject_index + o_idx]
                 number_of_triples = random.randrange(INSERT_DATA_TEMPLATE_SIZE_MIN, INSERT_DATA_TEMPLATE_SIZE_MAX)
-                ntriples_for_operation = generate_triples(number_of_triples, subject, INSERT_INTEGER)
-                INSERT_INTEGER += number_of_triples
-                operation_name = "GSP_POST"
-                gsp_post(subject, ntriples_for_operation)
+                ntriples_for_operation = generate_triples(number_of_triples, subject, insert_integer)
+                insert_integer += number_of_triples
+                rdfstore.gsp_post(subject, ntriples_for_operation)
 
             # Graph Store Protocol: PUT
-            elif op == 3:
+            elif op_kind == OperationKind.GSP_PUT:
                 # select and already used subject
-                subject = subjects[random.randrange(0, sujbect_index+o_idx)]
+                subject = subjects[random.randrange(0, subject_index + o_idx)]
                 number_of_triples = random.randrange(INSERT_DATA_TEMPLATE_SIZE_MIN, INSERT_DATA_TEMPLATE_SIZE_MAX)
-                ntriples_for_operation = generate_triples(number_of_triples, subject, INSERT_INTEGER)
-                INSERT_INTEGER += number_of_triples
-                operation_name = "GSP_PUT"
-                gsp_put(subject, ntriples_for_operation)
+                ntriples_for_operation = generate_triples(number_of_triples, subject, insert_integer)
+                insert_integer += number_of_triples
+                rdfstore.gsp_put(subject, ntriples_for_operation)
 
             # Graph Store Protocol: DELETE
-            elif op == 4:
+            elif op_kind == OperationKind.GSP_DELETE:
                 # select and already used subject
-                subject = subjects[random.randrange(0, sujbect_index+o_idx)]
-                operation_name = "GSP_DELETE"
-                gsp_delete(subject)
-                
+                subject = subjects[random.randrange(0, subject_index + o_idx)]
+                ntriples_for_operation = None
+                rdfstore.gsp_delete(subject)
+
             else:
                 raise "Error: Operation number out of expected range"
             
-            validation_ntriples_in_default_graph = validation_default_graph(subject)
-            validation_ntriples_in_named_graph = validation_named_graph(subject)
+            validation_triples_in_default_graph = rdfstore.validation_default_graph(subject)
+            validation_triples_in_named_graph = rdfstore.validation_named_graph(subject)
 
-            operantion_file = f"{worker_dir}/op_{o_idx}.json"
-            json_as_dict = {
-                "subject": subject,
-                "operation": operation_name,
-                "new_triples": ntriples_for_operation,
-                "validation_default": validation_ntriples_in_default_graph,
-                "validation_named": validation_ntriples_in_named_graph,
-            }
-            with open(operantion_file, "w") as output_file:
-                output_file.write(json.dumps(json_as_dict, indent=4))
+            operation = UpdateOperation(
+                subject=subject,
+                operation=op_kind,
+                triples=ntriples_for_operation,
+                validation_default=validation_triples_in_default_graph,
+                validation_named=validation_triples_in_named_graph,
+            )
 
-        sujbect_index += N_OPERATIONS
+            with open(f"{worker_dir}/op_{o_idx}.json", "w") as output_file:
+                output_file.write(operation.to_json())
 
+        subject_index += N_OPERATIONS
